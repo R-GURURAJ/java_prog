a30731f532a862075544f5b8848469e69bfd6e40
R-GURURAJ <guru1guru0@gmail.com>
2022-11-05 09:46:18 +0530

    change

---
+++ STRING_JAVA/swap_with_sequence_number.java
@@ -0,0 +1,11 @@
+
+
+public class swap_with_sequence_number {
+    public static void main(String[] args) {
+        String qus= "Today is preasure";    // Is yadto erusaerp
+        String arr[] = new String[qus.length()];
+        for (int i = 0; i < qus.length(); i++) {
+            
+        }
+    }    
+}
