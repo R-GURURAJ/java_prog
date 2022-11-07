4c6794c5e47a961cc5cd9b5f1995bb22c3e29659
R-GURURAJ <guru1guru0@gmail.com>
2022-11-07 19:15:19 +0530

    ignore

--- leetCoad/matrix_path.java
+++ leetCoad/matrix_path.java
@@ -4,14 +4,23 @@
 public class matrix_path {
 
     public static void main(String[] args) {
-        
+        matrix_path kk= new matrix_path();
         int s[][]= {{1,-2,1},{1,-2,1},{3,-4,1}};
         System.out.println(s.length);
         int sum=0;
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < s.length; j++) {
-                System.out.println(i+"  "+j+"  "+s[i][j]);
+                kk.path(j,i,s);
             }         
         }
+    }
+    public int path(int i,int j,int[][] s) {
+        int x=0;
+        System.out.println(i+"  "+j+"  ");
+        for (int j2 = 0; j2 < s.length; j2++) {
+            System.out.println(s[j2][j]);
+        }
+        System.out.println("-------------------");
+        return 0;
     }
 }
